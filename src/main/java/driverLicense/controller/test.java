5b7e0fa6957e4412c05bf782ed7977f2f36926c0
roarquokkaking <ldw9581@naver.com>
2024-06-05 16:06:29 +0900

    444444444

--- src/main/java/driverLicense/controller/test.java
+++ src/main/java/driverLicense/controller/test.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class test {
 
-    @GetMapping(path="test")
+    @GetMapping(path="/")
     public String test(){
         return "asd";
     }
